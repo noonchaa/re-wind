4c55c170748e65566774d7f1863a8c1d777aa966
Yono Raphael <enouchiha11@gmail.com>
2022-01-21 16:32:17 +0700

    Updates

---
+++ precache-manifest.49026df43625630ce664afac94b84604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236d661db18233c625b92b544995277e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ae241f9c7c8e05682b6",
+    "url": "./static/css/main.e216e8cc.chunk.css"
+  },
+  {
+    "revision": "de8615eca257341f2e65",
+    "url": "./static/js/2.fcd5cff3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.fcd5cff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae241f9c7c8e05682b6",
+    "url": "./static/js/main.1e75d967.chunk.js"
+  },
+  {
+    "revision": "f7630f924cde6773d260",
+    "url": "./static/js/runtime-main.107a0bf9.js"
+  }
+]);
